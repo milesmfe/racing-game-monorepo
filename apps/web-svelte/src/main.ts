13d56ae8502384766323fdf71245c2be444629bd
Miles Edwards <miles.mfe@gmail.com>
2025-10-16 00:26:01 +0100

    Add TrackDesigner user interface (temporary implementaiton)

--- apps/web-svelte/src/main.ts
+++ apps/web-svelte/src/main.ts
@@ -1,9 +1,17 @@
-import { mount } from 'svelte'
-import App from './App.svelte'
-import "@lib/styles/global.css"
+import { mount } from "svelte";
+import App from "./App.svelte";
+import TrackDesigner from "./TrackDesigner.svelte";
+import "@lib/styles/global.css";
+import "@lib/styles/track-designer.css";
 
 const app = mount(App, {
-  target: document.getElementById('app')!,
-})
+  target: document.getElementById("app")!,
+});
 
-export default app
+const designer = mount(TrackDesigner, {
+  target: document.getElementById("designer")!,
+});
+
+export { designer };
+
+export default app;
